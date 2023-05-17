cae4d1f353e5b8f57a9128e6cf13915896008526
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-05-17 21:11:22 +0100

    The code fech API is added.

--- script.js
+++ script.js
@@ -18,6 +18,31 @@ function searchMeal(event) {
 
   // Check for empty
   if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+            <div class="meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+              <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>
+          `
+            )
+            .join("");
+        }
+      });
+    // Clear search text
+    search.value = "";
   } else {
     showNotification();
     console.log("clicked");
@@ -33,3 +58,13 @@ function showNotification() {
 
 // Event listeners
 submit.addEventListener("submit", searchMeal);
+
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+});
