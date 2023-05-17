5dabaf9de06efde3e5cefba08df631e5c05a502f
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-05-17 04:41:40 +0100

    The notification popup for empty search is added.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-fetch().then().then();
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const random = document.getElementById("random");
+const mealsEl = document.getElementById("meals");
+const resultHeading = document.getElementById("result-heading");
+const single_mealEl = document.getElementById("single-meal");
+const notification = document.getElementById("notification-container");
+
+// Search the meal and fetch from API
+function searchMeal(event) {
+  event.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = "";
+
+  // Get search term
+  const term = search.value;
+
+  // Check for empty
+  if (term.trim()) {
+  } else {
+    showNotification();
+    console.log("clicked");
+  }
+}
+
+function showNotification() {
+  notification.classList.add("show");
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+// Event listeners
+submit.addEventListener("submit", searchMeal);
